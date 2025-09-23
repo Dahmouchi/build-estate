6c289d5a3a9c6852e1ace38bb17f36b496d41726
Elhassan_dahmouchi <hassandahmouchi0@gmail.com>
2025-09-23 10:48:45 +0100

    test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,20 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
- 
+ async headers() {
+    return [
+      {
+        // Apply to all API routes (you can restrict to just /api/reservations if you want)
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // ❗ Replace "*" with your domain in production
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,POST,PUT,DELETE" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ],
+      },
+    ]
+  },
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
